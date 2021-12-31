c3b6ee726419b88fabc1bdfbee2c4e4db9c3683f
AbdeljalilElhassani <90702628+AbdeljalilElhassani@users.noreply.github.com>
2021-12-31 23:45:49 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+
+$("#firstBtn").click(function() {
+  $([document.documentElement, document.body]).animate({
+      scrollTop: $("#firstSection").offset().top
+  }, 2000);
+});
+$("#secondBtn").click(function() {
+  $([document.documentElement, document.body]).animate({
+      scrollTop: $("#secondSection").offset().top
+  }, 2000);
+});
+$("#thirdBtn").click(function() {
+  $([document.documentElement, document.body]).animate({
+      scrollTop: $("#thirdSection").offset().top
+  }, 2000);
+});
+$("#fourthBtn").click(function() {
+  $([document.documentElement, document.body]).animate({
+      scrollTop: $("#fourthSection").offset().top
+  }, 2000);
+});
+$("#fifthBtn").click(function() {
+  $([document.documentElement, document.body]).animate({
+      scrollTop: $("#fifthSection").offset().top
+  }, 2000);
+});
+$("#lastBtn").click(function() {
+  $([document.documentElement, document.body]).animate({
+      scrollTop: $("#lastSection").offset().top
+  }, 2000);
+});
+
+
+
